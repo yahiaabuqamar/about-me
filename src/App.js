a16b5ff15f0b9c484a585ff25c0d67c437bcead0
yahiaabuqamar <yahiaabuqamar2004@gmail.com>
2021-06-24 15:04:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src = "/images/img1.jpg" alt = ""/>
+        <p>
+          Yahia Abuqamar
+        </p>
+        <p>
+          Im 17 years old 
+        </p>
+        <p>Real Madrid lover</p>
+        <p>
+          Crazy about Football & Learning new languages
+        </p>
+        <p>
+        In my spare time I play video games, especially PES
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/profile.php?id=100027354229271"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My profile page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
